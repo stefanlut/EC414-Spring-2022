95cd5b6d5be852f3455c23dd03de8090f83b16e9
Stefan Lutschg <hirzli2@gmail.com>
2022-05-02 16:00:34 -0400

    Finished Homework 9 Code

--- slutschg_hw9_3.m
+++ slutschg_hw9_3.m
@@ -14,13 +14,24 @@ y_train = model(trainData(:,1:2)');
 train_confmat = confusionmat(trainData(:,3),train_scores);
 train_ccr = trace(train_confmat)/length(trainData);
 train_perf = perform(model,trainData(:,3)',y_train);
+figure;
+gscatter(trainData(:,1),trainData(:,2),train_scores); grid on;
+legend("Predicted Training Class 1","Predicted Training Class 2");
+xlabel('X1');
+ylabel("X2");
+title("Predicted Classes using Training Dataset")
 %% 2 Neuron Classifier - Test
 y_test = model(testData(:,1:2)');
 [~,test_scores] = max(y_test);
 test_confmat = confusionmat(testData(:,3),test_scores);
 test_ccr = trace(test_confmat)/length(testData);
 test_perf = perform(model,testData(:,3)',y_test);
-
+figure;
+gscatter(testData(:,1),testData(:,2),test_scores); grid on;
+legend("Predicted Test Class 1","Predicted Test Class 2");
+xlabel('X1');
+ylabel("X2");
+title("Predicted Classes using Test Dataset")
 %% 10 Neuron Classifier - Train
 clearvars -except testData trainData
 clc,close all;
@@ -35,9 +46,21 @@ y_train = model(trainData(:,1:2)');
 train_confmat = confusionmat(trainData(:,3),train_scores);
 train_ccr = trace(train_confmat)/length(trainData);
 train_perf = perform(model,trainData(:,3)',y_train);
+figure;
+gscatter(trainData(:,1),trainData(:,2),train_scores); grid on;
+legend("Predicted Training Class 1","Predicted Training Class 2");
+xlabel('X1');
+ylabel("X2");
+title("Predicted Classes using Training Dataset")
 %% 10 Neuro Classifier - Test
 y_test = model(testData(:,1:2)');
 [~,test_scores] = max(y_test);
 test_confmat = confusionmat(testData(:,3),test_scores);
 test_ccr = trace(test_confmat)/length(testData);
 test_perf = perform(model,testData(:,3)',y_test);
+figure;
+gscatter(testData(:,1),testData(:,2),test_scores); grid on;
+legend("Predicted Test Class 1","Predicted Test Class 2");
+xlabel('X1');
+ylabel("X2");
+title("Predicted Classes using Test Dataset")
