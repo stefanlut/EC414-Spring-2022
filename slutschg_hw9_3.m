036438f32c24c0900da9b865beb7c6fe5875f1e9
Stefan Lutschg <hirzli2@gmail.com>
2022-05-02 15:44:30 -0400

    Finished it I think

--- slutschg_hw9_3.m
+++ slutschg_hw9_3.m
@@ -2,44 +2,42 @@
 clear,clc,close all;
 load("nn-train.mat");
 load("nn-test.mat");
-% for i = 1:length(trainData)
-%     if(trainData(i,3) == 2)
-%        trainData(i,3) = -1;
-%     end
-% end
-% for i = 1:length(testData)
-%     if(testData(i,3) == 2)
-%        testData(i,3) = -1;
-%     end
-% end
 %% 2 Neuron Classifier - Train
 model = patternnet(2);
 model.divideParam.trainRatio = 1;
 model.divideParam.valRatio = 0;
 model.divideParam.testRatio = 0;
-model = train(model,trainData(:,1:2)',trainData(:,3)');
+model = train(model,trainData(:,1:2)',dummyvar(trainData(:,3))');
 view(model);
 y_train = model(trainData(:,1:2)');
-
-train_perf = perform(model,trainData(:,3)',y_train)
-train_classes = vec2ind(y_train);
+[~,train_scores] = max(y_train);
+train_confmat = confusionmat(trainData(:,3),train_scores);
+train_ccr = trace(train_confmat)/length(trainData);
+train_perf = perform(model,trainData(:,3)',y_train);
 %% 2 Neuron Classifier - Test
 y_test = model(testData(:,1:2)');
-test_perf = perform(model,testData(:,3)',y_test)
-test_classes = vec2ind(y_test);
+[~,test_scores] = max(y_test);
+test_confmat = confusionmat(testData(:,3),test_scores);
+test_ccr = trace(test_confmat)/length(testData);
+test_perf = perform(model,testData(:,3)',y_test);
 
-%% 10 Neuron Classifier
+%% 10 Neuron Classifier - Train
 clearvars -except testData trainData
 clc,close all;
 model = patternnet(10);
 model.divideParam.trainRatio = 1;
 model.divideParam.valRatio = 0;
 model.divideParam.testRatio = 0;
-model = train(model,trainData(:,1:2)',trainData(:,3)');
+model = train(model,trainData(:,1:2)',dummyvar(trainData(:,3))');
 view(model);
 y_train = model(trainData(:,1:2)');
+[~,train_scores] = max(y_train);
+train_confmat = confusionmat(trainData(:,3),train_scores);
+train_ccr = trace(train_confmat)/length(trainData);
+train_perf = perform(model,trainData(:,3)',y_train);
+%% 10 Neuro Classifier - Test
 y_test = model(testData(:,1:2)');
-train_perf = perform(model,trainData(:,3)',y_train)
-test_perf = perform(model,testData(:,3)',y_test)
-train_classes = vec2ind(y_train);
-test_classes = vec2ind(y_test);
+[~,test_scores] = max(y_test);
+test_confmat = confusionmat(testData(:,3),test_scores);
+test_ccr = trace(test_confmat)/length(testData);
+test_perf = perform(model,testData(:,3)',y_test);
